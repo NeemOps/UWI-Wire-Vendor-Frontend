5771f9bc0b53e45de5d139f4d97a3f2e92d775b0
Bradley <bradleyjesseb@gmail.com>
2023-03-20 17:33:35 -0300

    minor

--- uwiwire_vendor/lib/constants.dart
+++ uwiwire_vendor/lib/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color.fromARGB(255, 11, 57, 208);
+const kBackgroundColor = Color.fromARGB(255, 11, 57, 208);
 const kTextColor = 0;
-const kBackgroundColor = Colors.white;
+const kPrimaryColor = Colors.white;
 const kGrey = Color.fromARGB(255, 234, 236, 237);
 
 const double kDefaultPadding = 20.0;
